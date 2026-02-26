0a978c3cd1f52c70bdab084a6b7fab50251cd6e7
arpankap <akapoor1204@gmail.com>
2026-02-26 14:27:02 -0800

    Complete lab 6

--- Lab-6/lab_6_util.py
+++ Lab-6/lab_6_util.py
@@ -4,28 +4,11 @@ from contextlib import contextmanager
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
-def visualize_samples(samples, labels, title="Samples visualization", colors=["green", "orange"]):
-    """ Visualize first three dimensions of samples. """
-    # Convert colors to NumPy array
-    colors = np.array(colors)
-    # Create figure
-    fig = plt.figure(figsize=(20, 5))
-    
-    # Plot first two dimensions on a 2D plot
-    ax_2d = fig.add_subplot(1, 2, 1)
-    ax_2d.set_title(f"{title} [2D]")
-    ax_2d.scatter(samples[:, 0], samples[:, 1], c=colors[labels])
-    
-    # Plot first three dimensions on a 3D plot
-    ax_3d = fig.add_subplot(1, 2, 2, projection="3d")
-    ax_3d.set_title(f"{title} [3D]")
-    ax_3d.scatter(samples[:, 0], samples[:, 1], samples[:, 2], c=colors[labels])
-    
-    print(fig)
+from sklearn.metrics import classification_report, confusion_matrix
 
 @contextmanager
 def timeit(action="Timing"):
+    """ Print the execution time of certain Python operations. """
     # Record start time
     print(f"{action} started...")
     start_time = time.time()
@@ -35,4 +18,18 @@ def timeit(action="Timing"):
     
     # Compute and show elapsed time
     elapsed_time = time.time()-start_time
-    print(f"{action} completed. Elapsed time: {elapsed_time:.2f}s")
+    print(f"{action} completed. Elapsed time: {elapsed_time:.2f}s\n")
+
+def evaluate_model(model, name, feat_test, y_test):
+    """ Evaluate a classification model on the test set, then print and plot metrics. """
+    # Make prediction from features
+    pred_test = model.predict(feat_test)
+    
+    print(f"[ Evaluation result for {name} ]")
+    # Print classification report
+    print("Classification report:")
+    print(classification_report(y_test, pred_test))
+    
+    # Print confusion matrix
+    print("Confusion matrix:")
+    print(confusion_matrix(y_test, pred_test), "\n")
